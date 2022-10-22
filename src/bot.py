1ad89f406ee7eed9721d33bb54512cc8261648f1
martinlaursen97 <martin_laursen21@hotmail.com>
2022-10-22 22:46:17 +0200

    discord setup

--- src/bot.py
+++ src/bot.py
@@ -4,9 +4,27 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+
 def run():
     TOKEN = os.getenv('TOKEN')
+    CHANNEL = os.getenv('CHANNEL')
+
+    intents = discord.Intents.default()
+    intents.messages = True
+
+    client = discord.Client(intents=intents)
+
+    @client.event
+    async def on_ready():
+        print(f'Logged in as {client.user}')
+
+    @client.event
+    async def on_message(message):
+        if message.author == client.user:
+            return
+
+    client.run(TOKEN)
 
 
-
-run()
+if __name__ == '__main__':
+    run()
